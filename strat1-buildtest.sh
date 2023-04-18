34a628bf71c38677ff88477f4e951ba0ef1d2489
Dylan Cross <git@dylancross.nz>
2023-04-18 12:00:34 +1200

    Update and rename buildtest.sh to strat1-buildtest.sh

--- strat1-buildtest.sh
+++ strat1-buildtest.sh
@@ -5,3 +5,7 @@ mkdir /var/www/public/generated_dir
 echo "Touching file inside generated_dir"
 echo "Build script generated file" > /var/www/public/generated_dir/file.txt
 
+echo "Running mkdir generated_dir"
+mkdir /mnt/data/shared/generated_dir
+echo "Touching file inside generated_dir"
+echo "Build script generated file" > /mnt/data/shared/generated_dir/file.txt
