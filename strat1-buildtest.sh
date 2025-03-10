243e652633098049e2d47c5659f4092444c53dad
Flynn <fbody106@gmail.com>
2025-03-10 13:33:06 +1300

    Update strat1-buildtest.sh

--- strat1-buildtest.sh
+++ strat1-buildtest.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-echo "Running mkdir generated_dir"
-mkdir /var/www/public/generated_dir
-echo "Touching file inside generated_dir"
-echo "Build script generated file" > /var/www/public/generated_dir/file.txt
-
-echo "Running mkdir generated_dir"
-mkdir /mnt/data/shared/generated_dir
-echo "Touching file inside generated_dir"
-echo "Build script generated file" > /mnt/data/shared/generated_dir/file.txt
+env
